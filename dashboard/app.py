e4bd1b2ac1f6a9ced2cacd69db6cf095a59934bf
mauricio <mauroz9010@gmail.com>
2026-08-10 08:22:50 -0500

    Corrigiendo requirements y rutas de app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -94,4 +94,7 @@ registrar_callbacks(app, df)
 
 # Ejecutar la app
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Render asigna dinámicamente un puerto mediante variables de entorno
+    import os
+    port = int(os.environ.get("PORT", 8050))
+    app.run(debug=False, host='0.0.0.0', port=port)
